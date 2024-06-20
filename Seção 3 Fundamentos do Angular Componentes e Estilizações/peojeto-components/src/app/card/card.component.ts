1e5eb12e949d8252513a6d563cf46936c4c11e7c
@racinefell <102471647+racinefe@users.noreply.github.com>
2024-06-20 11:04:15 -0300

    encadeamento opcional, interpolação e template

--- Seção 3 Fundamentos do Angular Componentes e Estilizações/peojeto-components/src/app/card/card.component.ts
+++ Seção 3 Fundamentos do Angular Componentes e Estilizações/peojeto-components/src/app/card/card.component.ts
@@ -1,15 +1,24 @@
 import { Component } from '@angular/core';
 
+interface IPlano {
+  infos: IInfos;
+}
+
+interface IInfos {
+  tipo: string;
+  preco: number;
+}
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss'
 })
 export class CardComponent {
- plano = {
-  infos: {
-    tipo: 'Simples',
-    preco: 100,
-  }
- }
+ //@ts-ignore
+  plano: IPlano = {
+    infos: {
+      tipo: 'Simples',
+      preco: 100,
+    },
+  };
 }
